4f2698e4c1e02e86be58dce9f4a378f2d01e67d9
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2019-11-10 14:33:37 +0100

    Test demo boot

--- webapp/src/main/java/org/WebApplication.java
+++ webapp/src/main/java/org/WebApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
-    }
+
+            SpringApplication.run(WebApplication.class, args);
+        }
+
 }
---
+++ webapp/src/main/java/org/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package org.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.model.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping("/")
+    public List<Employee> getEmployees()
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		return employeesList;
+    }
+
+}
